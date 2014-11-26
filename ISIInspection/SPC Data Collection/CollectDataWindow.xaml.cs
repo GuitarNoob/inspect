c5eb02f394bb50ecb499c108192946c2245592c6
GuitarNoob <paulep11@gmail.com>
2014-11-25 23:04:06 -0800

    need to finish collection window

---
+++ ISIInspection/SPC Data Collection/CollectDataWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ISIInspection.Models;
+using MieTrakWrapper.MieTrak;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SPC_Data_Collection
+{
+    /// <summary>
+    /// Interaction logic for CollectDataWindow.xaml
+    /// </summary>
+    public partial class CollectDataWindow : Window
+    {
+        public CollectDataWindow(WorkOrder workOrder, InspectionPlan inspectionPlan)
+        {
+            InitializeComponent();
+        }
+
+        void LoadRows(WorkOrder wo, InspectionPlan iplan)
+        {
+            string code = AQLSamplingHelper.GetSampleSizeCode((int)(wo.QuantityRequired ?? 0), (InspectionLevel)Enum.Parse(typeof(InspectionLevel), iplan.Level));
+            int samplingSize = -1;
+            int samplingError = -1;
+            AQLSamplingHelper.GetSampleSize(code, iplan.AQLPercentage, out samplingSize, out samplingError);
+        }
+    }
+}
