3f131a141a7c1c10189423578db21736851879f6
GuitarNoob <paulep11@gmail.com>
2014-11-26 16:46:59 -0800

    updates to collection window

--- ISIInspection/SPC Data Collection/CollectDataWindow.xaml.cs
+++ ISIInspection/SPC Data Collection/CollectDataWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MieTrakWrapper.MieTrak;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,22 +17,48 @@ using System.Windows.Shapes;
 
 namespace SPC_Data_Collection
 {
-    /// <summary>
-    /// Interaction logic for CollectDataWindow.xaml
-    /// </summary>
-    public partial class CollectDataWindow : Window
-    {
-        public CollectDataWindow(WorkOrder workOrder, InspectionPlan inspectionPlan)
-        {
-            InitializeComponent();
-        }
+   /// <summary>
+   /// Interaction logic for CollectDataWindow.xaml
+   /// </summary>
+   public partial class CollectDataWindow : Window
+   {
+      ObservableCollection<MeasurementCollector> measurementCollectors = new ObservableCollection<MeasurementCollector>();
 
-        void LoadRows(WorkOrder wo, InspectionPlan iplan)
-        {
-            string code = AQLSamplingHelper.GetSampleSizeCode((int)(wo.QuantityRequired ?? 0), (InspectionLevel)Enum.Parse(typeof(InspectionLevel), iplan.Level));
-            int samplingSize = -1;
-            int samplingError = -1;
-            AQLSamplingHelper.GetSampleSize(code, iplan.AQLPercentage, out samplingSize, out samplingError);
-        }
-    }
+      public CollectDataWindow(WorkOrder workOrder, InspectionPlan inspectionPlan)
+      {
+         InitializeComponent();
+      }
+
+      void LoadRows(WorkOrder wo, InspectionPlan iplan)
+      {
+         string code = AQLSamplingHelper.GetSampleSizeCode((int)(wo.QuantityRequired ?? 0), (InspectionLevel)Enum.Parse(typeof(InspectionLevel), iplan.Level));
+         int samplingSize = -1;
+         int samplingError = -1;
+         AQLSamplingHelper.GetSampleSize(code, iplan.AQLPercentage, out samplingSize, out samplingError);
+
+         foreach (PartMeasurementSP sp in iplan.MeasurementCriteria)
+         {
+            for (int i = 0; i < samplingSize; i++)
+               measurementCollectors.Add(new MeasurementCollector(sp));
+         }
+         DataGridMeasurements.ItemsSource = measurementCollectors;
+      }      
+
+      private void Window_Loaded(object sender, RoutedEventArgs e)
+      {
+
+      }
+   }
+
+   public class MeasurementCollector
+   {
+      public PartMeasurementSP SetPoint { get; set; }
+      public decimal Measured { get; set; }
+      public bool IsReadOnly = false;
+
+      public MeasurementCollector(PartMeasurementSP sp)
+      {
+         SetPoint = sp;
+      }
+   }   
 }
---
+++ ISIInspection/SPC Data Collection/MeasurementCollectionUserControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ISIInspection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPC_Data_Collection
+{
+   /// <summary>
+   /// Interaction logic for MeasurementCollectionUserControl.xaml
+   /// </summary>
+   public partial class MeasurementCollectionUserControl : UserControl
+   {
+      public MeasurementCollectionUserControl(PartMeasurementSP setPoint)
+      {
+         InitializeComponent();
+      }
+
+      public MeasurementCollectionUserControl(PartMeasurementSP setPoint, PartMeasurementActual actualValue)
+      {
+         InitializeComponent();
+      }
+   }
+}
