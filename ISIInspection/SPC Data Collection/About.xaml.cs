676155f8be3f5bc149f0da850687324c12962f83
saguilar131 <saguilar@merlinisi.com>
2014-12-18 18:16:09 -0800

    More Changes

--- ISIInspection/SPC Data Collection/About.xaml.cs
+++ ISIInspection/SPC Data Collection/About.xaml.cs
@@ -23,5 +23,10 @@ namespace SPC_Data_Collection
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
