a6d5663c746c5a428506a1e312a6e379ce22aa19
GuitarNoob <paulep11@gmail.com>
2014-12-18 20:35:24 -0800

    commit

--- ISIInspection/SPC Data Collection/CollectDataWindow.xaml.cs
+++ ISIInspection/SPC Data Collection/CollectDataWindow.xaml.cs
@@ -99,8 +99,8 @@ namespace SPC_Data_Collection
         {
             try
             {
-                if (((MeasurementCollector)e.Row.DataContext).IsReadOnly)
-                {
+                if (((MeasurementCollector)e.Row.DataContext).IsReadOnly == true)
+                {                    
                     e.Row.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFFFFBE"));
                 }
             }
